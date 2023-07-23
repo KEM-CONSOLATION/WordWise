4fa2cb0042077ecc5c67bf2c2519d27bd7afb48b
Kem-consolation <consolationlotachi@gmail.com>
2023-07-23 01:32:42 +0100

    Added some sections

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import styles from "./Button.module.css";
+const Button = ({ children, onClick, type }) => {
+  return (
+    <div>
+      <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
